7ae6c0fad693e7a6b1460df5734bd1653ac14edb
elenach <elena.cuevas@gmail.com>
2014-06-22 22:16:25 +0200

    V2

--- cachematrix.R
+++ cachematrix.R
@@ -4,20 +4,20 @@
 ## Write a short comment describing this function
 ## Create maxtrix to cache inverse
 makeCacheMatrix <- function(x = matrix()) {
-      #init
-      z <- NULL
-      #set the matrix
-      set <- function(y) {
-            x <<- y
-            z <<- NULL
-      }
-      #get the matrix
-      get <- function() x
+      #init save matrix & inverse_matrix to null
+      my_matrix<<-x
+      my_inv_matrix<<-NULL
       
-      #Set & get inverse, then list
-      setInverse <- function(inverse) z <<- inverse
-      getInverse <- function() z
-      list(set=set, get=get, setInverse=setInverse, getInverse=geIinverse)
+      #get matrix
+      getmatrix <- function() my_matrix
+      
+      #set the inverse of matrix
+      setInverse <- function(inv) my_inv_matrix <<-inv 
+      
+      getInverse <-function() my_inv_matrix
+      
+      #Return list of functions
+      list(getmatrix=getmatrix, setInverse=setInverse, getInverse=getInverse)
 
 }
 
